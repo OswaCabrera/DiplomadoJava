0d5f1d9f0e94a1f23daa90701b39afd27a2b0571
OswaCabrera <oswaldo_cabreracch@comunidad.unam.mx>
2023-10-29 18:57:54 -0600

    Práctica dos módulo 9

---
+++ M9_P2_Oswaldo_Cabrera/src/test/java/dgtic/core/M9_P2_Oswaldo_Cabrera/M9P2OswaldoCabreraApplicationTests.java
@@ -0,0 +1,13 @@
+package dgtic.core.M9_P2_Oswaldo_Cabrera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M9P2OswaldoCabreraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
