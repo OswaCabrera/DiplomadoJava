53749f23e01ece4f67744b54c608047b45b61383
OswaCabrera <oswaldo_cabreracch@comunidad.unam.mx>
2023-12-10 20:10:43 -0600

    Nuevos cambios

--- M9_PFinal_Cabrera_Oswaldo/src/main/java/mx/unam/dgtic/M9_PFinal_Cabrera_Oswaldo/repository/TransaccionRepositoryPageAndSortRepository.java
+++
@@ -1,7 +0,0 @@
-package mx.unam.dgtic.M9_PFinal_Cabrera_Oswaldo.repository;
-
-import mx.unam.dgtic.M9_PFinal_Cabrera_Oswaldo.model.Transaccion;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface TransaccionRepositoryPageAndSortRepository extends PagingAndSortingRepository <Transaccion, Integer>{
-}
--- M9_PFinal_Cabrera_Oswaldo/src/test/java/mx/unam/dgtic/M9_PFinal_Cabrera_Oswaldo/InfoTester.java
+++
@@ -1,9 +0,0 @@
-package mx.unam.dgtic.M9_PFinal_Cabrera_Oswaldo;
-
-public class InfoTester {
-    public static final String NOMBRE = """
-            ╔══════════════════════════════════╗
-            ║    Oswaldo Cabrera Pérez         ║
-            ╚══════════════════════════════════╝
-            """;
-}
