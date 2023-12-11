8390560763a7f3fe70d1f6b7bd58780e80758600
OswaCabrera <oswaldo_cabreracch@comunidad.unam.mx>
2023-12-10 20:27:16 -0600

    Agrego avance proyecto api

---
+++ caja-ahorro-api/src/main/java/unam/diplomado/cajaahorroapi/CajaAhorroApiApplication.java
@@ -0,0 +1,13 @@
+package unam.diplomado.cajaahorroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CajaAhorroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CajaAhorroApiApplication.class, args);
+	}
+
+}
---
+++ caja-ahorro-api/src/test/java/unam/diplomado/cajaahorroapi/CajaAhorroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package unam.diplomado.cajaahorroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CajaAhorroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
